ec19ef9068934131c79990b7cf27b0cd25b353c0
baiyajin <baiyajin@iv-auto.cn>
2020-08-04 11:36:33 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# baiyajin-vue-leaflet
+
+> baiyajin-vue-leaflet项目，可双击地图添加标记，点击标记弹出菜单，点击菜单弹出遮罩并跳转到指定的地图。
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+
+# run unit tests
+npm run unit
+
+# run e2e tests
+npm run e2e
+
+# run all tests
+npm test
+```
+
---
+++ src/components/VueLeaflet.vue
@@ -0,0 +1,204 @@
+<template>
+  <div class="vue-leaflet">
+    <l-map class="l-map" :zoom="map2.zoom" :center="map2.center" ref="map">
+      <l-tile-layer :url="map2.url" :attribution="map2.attribution"></l-tile-layer>
+      <l-marker v-for="(item, index) in map2.markers" :key="index" :lat-lng="item.marker">
+        <l-popup :content="item.text"></l-popup>
+      </l-marker>
+    </l-map>
+
+    <el-dialog
+      :title="map.title"
+      :close-on-click-modal="false"
+      :visible.sync="dialogVisible"
+      width="80%"
+      >
+      <div class="switch_div">
+        <div class="previous switch" @click="switchMap(0)">
+          <i class="el-icon-arrow-left"></i>
+        </div>
+        <l-map style="width: 100%; height: 600px;" :zoom="map.zoom" :center="map.center" ref="child_map">
+          <l-tile-layer :url="map.url" :attribution="map.attribution"></l-tile-layer>
+          <l-marker :lat-lng="map.marker">
+            <l-popup :content="map.text"></l-popup>
+          </l-marker>
+        </l-map>
+        <div class="next switch" @click="switchMap(1)">
+          <i class="el-icon-arrow-right"></i>
+        </div>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import { LMap, LTileLayer, LMarker, LPopup } from 'vue2-leaflet'
+import L from 'leaflet'
+export default {
+  name: 'VueLeaflet',
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+    LPopup
+  },
+  data () {
+    return {
+      buttons: [],
+      currentIndex: 0,
+      currentMarker: '',
+      currentCenter: '',
+      dialogVisible: false,
+      map: {
+        title: '',
+        zoom: 13,
+        center: '',
+        url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+        marker: '',
+        text: ''
+      },
+      map2: {
+        zoom: 13,
+        center: L.latLng(25.085540595994082, 102.73151814937593),
+        url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+        markers: []
+      }
+    }
+  },
+  created () {
+    // 初始化地图默认标记
+    this.map2.markers.push(this.getJson(L.latLng(25.085540595994082, 102.73151814937593)))
+  },
+  mounted () {
+    let self = this
+    // 双击添加标记
+    self.$refs.map.mapObject.on('click', function (e) {
+      // 单击不添加
+    }).on('dblclick', function (e) {
+      // 双击才添加
+      self.map2.markers.push(self.getJson(L.latLng(e.latlng.lat, e.latlng.lng)))
+    })
+    self.$refs.map.mapObject.on('popupopen', function (e) {
+      self.currentCenter = self.currentMarker = L.latLng(e.popup._latlng.lat, e.popup._latlng.lng)
+      self.buttons = e.popup._contentNode.getElementsByClassName('my-custom-button')
+      for (let i = 0; i < self.buttons.length; i++) {
+        self.buttons[i].onclick = function () {
+          self.setMapParams(i)
+          self.dialogVisible = true
+        }
+      }
+    })
+  },
+  methods: {
+    getJson (marker) {
+      return {
+        marker: marker,
+        text: `<div class="title">子地图列表</div>` +
+          `<br><button class="my-custom-button">地图一</button>` +
+          `<br><button class="my-custom-button">地图二</button>` +
+          `<br><button class="my-custom-button">地图三</button>` +
+          `<br><button class="my-custom-button">地图四</button>` +
+          `<br><button class="my-custom-button">地图五</button>` +
+          `<br><button class="my-custom-button">地图六</button>`
+      }
+    },
+    setMapParams (index) {
+      this.currentIndex = index
+      this.map.center = this.currentCenter
+      this.map.marker = this.currentMarker
+      this.map.zoom = index + 13
+      this.map.text = `<div class="title">我是 ${this.buttons[index].innerHTML}</div>`
+      this.map.title = `${this.buttons[index].innerHTML} ------- 缩放(${this.map.zoom}) ------- 位置(${this.map.marker})`
+    },
+    switchMap (flag) {
+      // 计算当前index
+      if (flag === 0) {
+        if (this.currentIndex === 0) {
+          this.currentIndex = this.buttons.length - 1
+        } else {
+          this.currentIndex--
+        }
+      } else {
+        if (this.currentIndex === this.buttons.length - 1) {
+          this.currentIndex = 0
+        } else {
+          this.currentIndex++
+        }
+      }
+      this.setMapParams(this.currentIndex)
+    }
+  }
+}
+</script>
+
+<style lang="less" rel="stylesheet/scss" scoped>
+  .vue-leaflet{
+    top: 0;
+    left: 0;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    .l-map{
+      width: 100%;
+      height: 100%;
+      position:absolute;
+    }
+    /deep/.leaflet-popup-content{
+      width: 100% !important;
+      .title{
+        position: relative;
+        left: -19px;
+        top: 10px;
+        text-align: center;
+        padding: 10px 20px;
+      }
+      button{
+        margin: 10px auto;
+        display: inline-block;
+        line-height: 1;
+        white-space: nowrap;
+        cursor: pointer;
+        background: #fff;
+        border: 1px solid #dcdfe6;
+        color: #606266;
+        -webkit-appearance: none;
+        text-align: center;
+        box-sizing: border-box;
+        outline: none;
+        transition: .1s;
+        font-weight: 500;
+        -moz-user-select: none;
+        -webkit-user-select: none;
+        -ms-user-select: none;
+        padding: 12px 20px;
+        font-size: 14px;
+        border-radius: 4px;
+        &:hover {
+          color: #409eff;
+          border-color: #c6e2ff;
+          background-color: #ecf5ff;
+        }
+      }
+    }
+    .switch_div{
+      .switch{
+        position: absolute;
+        top: 50%;
+        color: white;
+        font-size: 50px;
+        &:hover {
+          color: #5cb6ff;
+          transform: scale(1.2);
+        }
+      }
+      .previous{
+        left: -9%;
+      }
+      .next{
+        right: -9%;
+      }
+    }
+  }
+</style>
