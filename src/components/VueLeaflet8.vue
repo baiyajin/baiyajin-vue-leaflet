07719161907843d4bf798ed2a6ee833c31cdefe1
baiyajin <baiyajin@iv-auto.cn>
2020-08-09 15:02:15 +0800

    增加demo8

---
+++ src/components/VueLeaflet8.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="vue-leaflet">
+    <div id="map"></div>
+  </div>
+</template>
+<script>
+  import('@/assets/js/rastercoords')
+  import('@/assets/js/geojson')
+  import('@/assets/js/index')
+</script>
+<style lang="less" rel="stylesheet/scss" scoped>
+  html, body, #map {
+    width:100%;
+    height:100%;
+    margin:0;
+    padding:0;
+    background-color: #B0B0B0
+  }
+</style>
